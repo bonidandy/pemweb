fc750d33b43355546e22856c1c1bdb532d43be30
bonidandy <bonidandy@gmail.com>
2023-04-28 20:34:35 +0700

    pert

--- pert3/src/resources/views/sample.blade.php
+++ pert3/src/resources/views/sample.blade.php
@@ -10,7 +10,7 @@
     text-align: center;
   }
 </style>
-<div class="card" style="width: 18rem; ">
+<div class="card offset-4" style="width: 18rem; ">
   <img src="https://marketplace.canva.com/EAFEits4-uw/1/0/400w/canva-boy-cartoon-gamer-animated-twitch-profile-photo-Pk-dGK9pdQg.jpg" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">Dandy</h5>
